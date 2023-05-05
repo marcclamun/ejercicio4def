cc1c5037039ad1e9c24b26ed3b1ce5944fcdc821
marcclamun <marcclamun@monlau.com>
2023-05-05 17:10:35 +0200

    h

--- src/App.js
+++ src/App.js
@@ -7,33 +7,35 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
 import Header2 from './Header2';
+
 import './App.css';
 
 
 
 
 function App() {
-  
 
-  return (
-    <div className='header'>
-      <Header2 />
-      <Router>
-        <div className='h'>
-          <Link className="nav-link " to={'/Tamagotchi'}>
-            {/* Home */}
-          </Link>
-          <Link className="nav-link " to={'/Piedra'}>
-            {/* Home */}
-          </Link>
-          <Routes>
-            <Route exact path="/Tamagotchi" element={<Tamagotchi />} />
-            <Route exact path="/Piedra" element={<Piedra />} />
-          </Routes>
+
+    return (
+        <div className='header'>
+            <Header2 />
+            <Router>
+                <div className='h'>
+                    <Link className="nav-link " to={'/Tamagotchi'}>
+                        {/* Home */}
+                    </Link>
+                    <Link className="nav-link " to={'/Piedra'}>
+                        {/* Home */}
+                    </Link>
+                    <Routes>
+                        <Route exact path="/Tamagotchi" element={<Tamagotchi />} />
+                        <Route exact path="/Piedra" element={<Piedra />} />
+                    </Routes>
+                </div>
+                
+            </Router>
         </div>
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default App;
